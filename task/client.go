cb7708c1422f3dd04e4bd8834b08c92baa48c7b2
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-10-07 21:02:27 +0200

    add DisplayTasks method

--- task/client.go
+++ task/client.go
@@ -3,11 +3,16 @@ package task
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
+	"sort"
+	"text/tabwriter"
 )
 
+type Tasks []Task
+
 type Client struct {
-	tasks []Task
+	tasks Tasks
 }
 
 func (c *Client) AddTask(task Task) {
@@ -57,6 +62,32 @@ func (c *Client) UpdateTaskDescription(taskID int, description string) error {
 	return nil
 }
 
+func (c *Client) DisplayTasks(sortPriority bool, displayPriority bool) {
+	if len(c.tasks) == 0 {
+		fmt.Println("Nothing on your To-Do list for the moment.")
+	} else {
+		w := tabwriter.NewWriter(os.Stdout, 4, 0, 1, ' ', 0)
+		if displayPriority {
+			fmt.Fprintln(w, "ID\tTask\tStatus\tPriority")
+			fmt.Fprintln(w, "--\t----\t------\t--------")
+		} else {
+			fmt.Fprintln(w, "ID\tTask\tStatus")
+			fmt.Fprintln(w, "--\t----\t------")
+		}
+		if sortPriority {
+			sort.Sort(ByPriority(c.tasks))
+		}
+		for _, t := range c.tasks {
+			if displayPriority {
+				fmt.Fprintln(w, t.PrettyPosition()+"\t"+t.Description+"\t"+t.PrettyStatus()+"\t"+t.PrettyPriority())
+			} else {
+				fmt.Fprintln(w, t.PrettyPosition()+"\t"+t.Description+"\t"+t.PrettyStatus())
+			}
+		}
+		w.Flush()
+	}
+}
+
 func (c *Client) ReadTasks(filename string) error {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -84,22 +115,22 @@ func (c *Client) SaveTasks(filename string) error {
 	return nil
 }
 
-type ByPriority Client
+type ByPriority Tasks
 
 func (s ByPriority) Len() int {
-	return len(s.tasks)
+	return len(s)
 }
 
 func (s ByPriority) Swap(i, j int) {
-	s.tasks[i], s.tasks[j] = s.tasks[j], s.tasks[i]
+	s[i], s[j] = s[j], s[i]
 }
 
 func (s ByPriority) Less(i, j int) bool {
-	if s.tasks[i].Done == s.tasks[j].Done {
-		if s.tasks[i].Priority == s.tasks[j].Priority {
-			return s.tasks[i].position < s.tasks[j].position
+	if s[i].Done == s[j].Done {
+		if s[i].Priority == s[j].Priority {
+			return s[i].position < s[j].position
 		}
-		return s.tasks[i].Priority < s.tasks[j].Priority
+		return s[i].Priority < s[j].Priority
 	}
-	return !s.tasks[i].Done
+	return !s[i].Done
 }
