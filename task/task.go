62abf986914e1f85938c8d0865ae08bc9e8ce2c5
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-10-09 20:20:17 +0200

    add error handling to SetPriority feature

--- task/task.go
+++ task/task.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"errors"
 	"strconv"
 )
 
@@ -11,8 +12,12 @@ type Task struct {
 	position    int
 }
 
-func (t *Task) SetPriority(priority int) {
+func (t *Task) SetPriority(priority int) error {
+	if priority != 1 && priority != 2 && priority != 3 {
+		return errors.New("invalid priority: must be 1, 2, or 3")
+	}
 	t.Priority = priority
+	return nil
 }
 
 func (t *Task) SetDone() {
